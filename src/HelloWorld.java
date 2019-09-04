ac815ef82a80aca30c33ce3e86d0d4af56dc8c5b
playrey2010 <playrey2010@gmail.com>
2019-09-04 11:14:42 -0400

    my first Java program

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Today is September 4, 2019. I am alive!");
+        System.out.println("My name is Rey");
+    }
+}
